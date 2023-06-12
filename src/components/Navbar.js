3c971aa6191789b73ba1266385120c4d65b0d5c2
Momo-87 <momochristian87@gmail.com>
2023-06-12 13:54:17 +0100

    Access store value, set up cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 
 const Navbar = () => {
   const { amount } = useSelector((state) => state.cart);
-
   return (
     <nav>
       <div className='nav-center'>
