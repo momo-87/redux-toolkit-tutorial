bae60e374ce6a4fef3009167ff8890bd320f47a8
Momo-87 <momochristian87@gmail.com>
2023-06-12 16:22:56 +0100

    Create a Redux logic directory and configure a Redux store

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
+
 
 const CartItem = ({ id, img, title, price, amount }) => {
+    const dispatch = useDispatch();
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
@@ -9,17 +13,23 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
         {/* remove button */}
-        <button className='remove-btn'>remove</button>
+        <button className='remove-btn' onClick={() => {
+            dispatch(removeItem(id));
+        }}>remove</button>
       </div>
       <div>
         {/* increase amount */}
-        <button className='amount-btn'>
-          <ChevronUp />
+        <button className='amount-btn' onClick={() => {
+            dispatch(increase({ id }));
+          }}>
+          <ChevronUp  />
         </button>
         {/* amount */}
         <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => {
+            dispatch(decrease({ id }));
+        }}>
           <ChevronDown />
         </button>
       </div>
